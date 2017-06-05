e5ffde29c6fec2cf90f6ea8c0e6f451214edd4ae
eligoodwin <goodwiel@oregonstate.edu>
2017-06-04 19:04:30 -0700

    Modded nearest
    .

--- PRO3/PRO3/main.cpp
+++ PRO3/PRO3/main.cpp
@@ -50,7 +50,7 @@ int main(int argc, const char * argv[]) {
     for(int i = 0; i < cityLength; ++i){
         cityCoordinates[i].distancesList = new int[cityLength];
     }
-        
+    
     //calc the distances
     makeDistances(cityCoordinates, cityLength);
     printTour(cityCoordinates, cityLength);
